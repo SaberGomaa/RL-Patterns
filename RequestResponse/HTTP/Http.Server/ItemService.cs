854a4fa4cb1166904f8c67e21742799bc28dd047
saber <sabergomaa261@gmail.com>
2024-09-17 11:50:42 +0300

    Add project files.

---
+++ RequestResponse/HTTP/Http.Server/ItemService.cs
@@ -0,0 +1,9 @@
+﻿namespace HTTP.Server;
+
+public class ItemService
+{
+    public async Task<Item> GetItem(int id)
+    {
+        return await Task.FromResult(new Item());
+    }
+}
---
+++ RequestResponse/gRPC/Grpc.Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Client;
+using Grpc.Net.Client;
+using Grpc.Server;
+using Microsoft.Extensions.DependencyInjection;
+
+var channel = GrpcChannel.ForAddress("https://localhost:5002");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = client.SayHello(new HelloRequest { Name = "World" });
+Console.WriteLine("Greeting: " + reply.Message);
+
+// Using the Client factory
+var services = new ServiceCollection();
+services.AddTransient<GreeterService>();
+services.AddGrpcClient<Greeter.GreeterClient>(o =>
+{
+    o.Address = new Uri("https://localhost:5002");
+});
+
+var serviceProvider = services.BuildServiceProvider();
+var greeterService = serviceProvider.GetRequiredService<GreeterService>();
+var serviceGetResponse = await greeterService.SayHelloAsync("World");
+
+Console.WriteLine(serviceGetResponse);
